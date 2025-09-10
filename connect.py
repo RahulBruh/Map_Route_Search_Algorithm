5dc4c1759095658fbd3dacd75045f45cd61b4cbe
SucculentGonads <rahulbabu.moka@gmail.com>
2025-09-10 09:35:56 -0500

    Update connect.py
    connected all the nodes into an adjacency list and also created a dictionary that stores the latitude and longitude as tuples.

--- connect.py
+++ connect.py
@@ -1,10 +1,30 @@
+from collections import defaultdict
 import xml.etree.ElementTree as ET
+class Node(object):
+    def __init__(self, val = 0, lat = 0, long = 0):
+        self.val = val
+        self.lat = lat
+        self.long = long
+        
+class Graph(object):
+    def connect(self, node):
+        roads = defaultdict(list)
+        coordinates = {}
 
+        w = 2
+
+        while root[w].tag == "way":
+            for nd in root[w]:
+                if nd.tag == "nd":
+                    roads[int(root[w].get("id"))].append(int(nd.get("ref")))
+            w += 1
+        
+        while w < len(root) and root[w].tag == "node":
+            coordinates[int(root[w].get("id"))] = (float(root[w].get("lat")), float(root[w].get("lon")))
+            w += 1
+
+        return coordinates
+    
 tree = ET.parse("Nashville_Chunck.osm")
 root = tree.getroot()
-
-print(root.tag)
-print(len(root))
-
-for i in range(len(root)):
-    print(root[i].tag)
+print(Graph().connect(root))
