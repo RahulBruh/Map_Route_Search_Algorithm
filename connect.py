dd235d924c34a3d00015ddd22a770e7d2afa1050
SucculentGonads <rahulbabu.moka@gmail.com>
2025-09-16 13:21:08 -0500

    Update connect.py
    Implemented a backtracking algorithm that shows the most optiomal path using bfs, this version ignores oneway raods.

--- connect.py
+++ connect.py
@@ -17,6 +17,7 @@ class Graph(Properites):
         roads = defaultdict(list)
         coordinates = {}
         oneway = set()
+        oneway_index = {}
 
         w = 2
 
@@ -46,7 +47,7 @@ class Graph(Properites):
 
                     elif i == nd_len - 1:
                         roads[int(root[w][i].get("ref"))].append(int(root[w][i - 1].get("ref")))
-                
+                 
 
             w += 1
         
@@ -54,34 +55,51 @@ class Graph(Properites):
             coordinates[int(root[w].get("id"))] = (float(root[w].get("lat")), float(root[w].get("lon")))
             w += 1
 
-        return roads
+        return roads, coordinates
 
 source = 202176692
 end = 10127810693
 
 
-def bfs(source, end, roads):
+def bfs(source, end, roads, coordinates):
     from collections import deque
     seen = set() 
     seen.add(source)
     q = deque()
     q.append(source)
+    parent = {source: None}
 
-
+    path = []
     while q:
         node = q.popleft()
         for nei_node in roads[node]:
             if nei_node not in seen:
                 seen.add(nei_node)
                 q.append(nei_node)
-                print(roads[node])
-                print("")
+                path.append(nei_node)
+                parent[nei_node] = node
                 if nei_node == end:
-                    return True
-    return False
+                    print(True)
+                    break
 
-            
-    
+    if end not in parent:
+        return []
+
+    path = []
+    cur = end
+    while cur is not None:
+        path.append(cur)
+        cur = parent[cur]
+    path.reverse()
+    return path, coordinates
+
 tree = ET.parse("Nashville_Chunck.osm")
 root = tree.getroot()
-print(bfs(202176692, 1240363910, Graph().connect(root)))
+filebruh = bfs(202176692, 7740969065, Graph().connect(root)[0], Graph().connect(root)[1])
+
+def write_gpx(path, coordinates):
+    for i in path:
+        print(f"<trkpt lat=\"{coordinates[i][0]}\" lon=\"{coordinates[i][1]}\"/>")
+            
+print(filebruh[0]) 
+
